563d21be366423913d008c3ac22c79d3f42ea6c1
Maciek Kumorek <mkumorek@gmail.com>
2021-11-23 00:32:05 -0800

    Frontend tree added

---
+++ app/src/App.tsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>Game 1</li>
+          <li>Game 2</li>
+        </ul>
+        <form>
+          <button>Create a game</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;

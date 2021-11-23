b013b6ca3902688f26598ef37d0395c6720ccd37
Maciek Kumorek <mkumorek@gmail.com>
2021-11-23 00:31:35 -0800

    Server folder

---
+++ server/go.mod
@@ -0,0 +1,2 @@
+module github.com/macqm/teexid
+
---
+++ server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func start() {
+
+}
+
+func main() {
+	flag.Parse()
+	log.Printf("Starting...")
+	start()
+}

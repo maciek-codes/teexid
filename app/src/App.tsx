e52c676ba4cf5f5cf4d256714fe2bb412e2d6285
Maciek Kumorek <mkumorek@gmail.com>
2021-12-18 10:57:30 -0800

    Introduce can connect state

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,24 +2,14 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import CreateRoomButton from './CreateRoomButton';
 import GameRoom from './GameRoom';
-import Player from './models/Player';
 import PlayerName from './PlayerName';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
+import RoomState from './models/RoomState';
 
 // Font awesome icons
 library.add(far);
-
-type GameState = 'waiting' | 'playing' | 'ended';
-
-export interface RoomState {
-  id: string,
-  playerId: string,
-  playerName: string,
-  players: Player[]
-  state: GameState
-}
 
 const initialState: RoomState = {
   id: '',
@@ -39,15 +29,12 @@ interface GameMessage {
 
 export type Action = {
   type: 'room/join' | 'room/create' | 'room/enter' |
-    'player/updateName' | 'player/ready',
+  'player/updateName' | 'player/ready',
   payload: any
 };
 
-function App() {
-  const ws = useRef<WebSocket|null>(null);
-
-  const [roomState, setRoomState] = useState<RoomState>(initialState);
-  const [joinStatus, setJoinStatus] = useState<JoinedStatus>('not_joined')
+const App = () => {
+  const ws = useRef<WebSocket | null>(null);
 
   const sendCommand = (action: Action) => {
     const cmd = {
@@ -59,64 +46,69 @@ function App() {
     ws.current?.send(commandStr);
   }
 
-  useEffect(() => {
-    try {
-      if (!ws.current && roomState.id) {
-        ws.current = new WebSocket(
-          'ws://localhost:8080/rooms/' 
-          + roomState.id 
-          + '?playerName=' + roomState.playerName );
+  const [canConnect, setCanConnect] = useState<Boolean>(false);
+  const [roomState, setRoomState] = useState<RoomState>(initialState);
+  const [joinStatus, setJoinStatus] = useState<JoinedStatus>('not_joined');
 
-        ws.current.onopen = (ev: Event) => {
-          console.log("Connected to the room.");
-        };
-
-        ws.current.onmessage = (ev: MessageEvent) => {
-          console.log("Message: " + ev.data);
-          const msg = JSON.parse(ev.data) as GameMessage;
-          switch (msg.type) {
-            case 'onjoined': {
-              setJoinStatus('joined');
-              setRoomState(prevState => {
-                return {
-                  ...prevState,
-                  id: msg.payload.roomId,
-                  playerId: msg.payload.playerId,
-                  isOwner: msg.payload.ownerId === msg.payload.playerId
-                }
-              });
-              break;
-            }
-            case 'onplayersupdated': {
-              setRoomState({ ...roomState, players: msg.payload.players });
-              break;
-            }
-            case 'onroomstateupdated': {
-              setRoomState({...roomState, state: msg.payload.state});
-              break;
-            }
-            default:
-              break;
+  const messageCallback = (msg: GameMessage) => {
+    switch (msg.type) {
+      case 'onjoined': {
+        setJoinStatus('joined');
+        setRoomState(prevState => {
+          return {
+            ...prevState,
+            id: msg.payload.roomId,
+            playerId: msg.payload.playerId,
+            isOwner: msg.payload.ownerId === msg.payload.playerId
           }
-        }
-
-        ws.current.onerror = (ev: Event) => {
-          console.error(ev);
-        }
-
-        ws.current.onclose = (ev: Event) => {
-          console.log("Closing");
-        }
+        });
+        break;
       }
-    } catch (err) {
-      console.error(err);
+      case 'onplayersupdated': {
+        setRoomState({ ...roomState, players: msg.payload.players });
+        break;
+      }
+      case 'onroomstateupdated': {
+        setRoomState({ ...roomState, state: msg.payload.state });
+        break;
+      }
+      default:
+        break;
     }
-  }, [roomState]);
+  };
+
+  useEffect(() => {
+    if (!ws.current && roomState.id && canConnect) {
+      ws.current = new WebSocket(
+        'ws://localhost:8080/rooms/'
+        + roomState.id
+        + '?playerName=' + roomState.playerName);
+
+      ws.current.onopen = (ev: Event) => {
+        console.log("Connected to the room.");
+      };
+
+      ws.current.onerror = (ev: Event) => {
+        console.error(ev);
+      }
+
+      ws.current.onclose = (ev: Event) => {
+        console.log("Closing");
+      }
+
+      ws.current.onmessage = (ev: MessageEvent) => {
+        console.log("Message: " + ev.data);
+        const msg = JSON.parse(ev.data) as GameMessage;
+        messageCallback(msg);
+      }
+    }
+  }, [canConnect, messageCallback, roomState.id, roomState.playerName]);
 
   const onCreateRoom = async () => {
     setJoinStatus("loading");
+    setCanConnect(true);
     try {
-      const response = await (await fetch('http://localhost:8080/rooms', { 
+      const response = await (await fetch('http://localhost:8080/rooms', {
         method: "POST",
       })).json();
       setRoomState({ ...roomState, id: response.id });
@@ -128,6 +120,7 @@ function App() {
 
   const onJoinRoom = (id: string) => {
     setJoinStatus("loading");
+    setCanConnect(true);
     setRoomState({ ...roomState, id: id });
   }
 
@@ -166,7 +159,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PlayerName sendCommand={sendCommand} playerName={roomState.playerName} 
+        <PlayerName sendCommand={sendCommand} playerName={roomState.playerName}
           onNameChanged={onPlayerNameChanged} />
         {entrace}
       </header>

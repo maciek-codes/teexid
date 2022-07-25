4e7d518c4a01d092134a07d763eb5e2349373b7c
Maciek Kumorek <mkumorek@gmail.com>
2022-07-25 00:31:48 -0700

    Update react

--- app/src/CreateRoomButton.tsx
+++ app/src/CreateRoomButton.tsx
@@ -1,27 +1,29 @@
+import { Button, Input, Stack } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 interface CreateRoomButtonProps {
-    createRoom(): void
-    joinRoom(id: string): void
 }
 
-const CreateRoomButton: React.FC<CreateRoomButtonProps> = ({createRoom, joinRoom}) => {
+const CreateRoomButton: React.FC<CreateRoomButtonProps> = ({ }) => {
     const [id, setId] = useState('');
 
     const createRoomClick = () => {
-        createRoom();
+        console.log("Create");
     }
 
     const joinRoomClick = () => {
-        joinRoom(id);
+        console.log("join");
     }
 
     return (
         <div className="flex flex-col space-y-4 p-6">
-            <button className="rounded-full bg-purple-700 text-white" onClick={createRoomClick}>Create room</button>
-            <input className="shadow appearance-none border rounded w-100 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" 
-                onChange={e => setId(e.target.value)}></input>
-            <button className="rounded-full bg-purple-700 text-white" onClick={joinRoomClick}>Join</button>
+            <Stack>
+                <Button onClick={createRoomClick} pb="10">Create room</Button>
+    
+                <Input type="text"
+                    onChange={e => setId(e.target.value)}></Input>
+                <Button className="rounded-full bg-purple-700 text-white" onClick={joinRoomClick}>Join</Button>
+            </Stack>
         </div>
     )
 }

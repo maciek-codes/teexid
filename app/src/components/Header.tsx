273d5e80f6da64e0bc86a7fd7426c82dcc221e04
Maciek Kumorek <mkumorek@gmail.com>
2023-01-14 15:18:14 -0800

    Move header to its own component

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Flex, Heading, Link, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import React from "react";
+import { CopyButton } from "./CopyButton";
+import { useRoom } from "../contexts/RoomContext";
+
+const Header: React.FC = () => {
+  const { roomId } = useRoom();
+  return (
+    <Heading size="xl" pt="10px" background="#48323A" px="0.5em" py="0.25em">
+      <Flex
+        flexDirection="row"
+        alignItems="start"
+        justifyItems="center"
+        justifyContent="space-between"
+      >
+        <Link as={RouterLink} to="/" color="#E7E2C1" mt="2">
+          TeeXid
+        </Link>
+
+        <Text fontSize="md" alignSelf="flex-end" color="white">
+          Room: {roomId} <CopyButton copyText={roomId} />
+        </Text>
+      </Flex>
+    </Heading>
+  );
+};
+
+export default Header;

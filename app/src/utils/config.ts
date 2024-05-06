b31bf03d72d2e2d25a5f92b97644664ff1af2882
Maciek Kumorek <mkumorek@gmail.com>
2024-05-05 23:23:29 -0700

    Fix host

--- app/src/utils/config.ts
+++ app/src/utils/config.ts
@@ -2,12 +2,12 @@ export const getWsHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "ws://localhost:4000/ws";
   }
-  return "wss://game.teexit.maciek.codes/ws";
+  return "wss://teexit.maciek.codes:8080/ws";
 };
 
 export const getHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "http://localhost:8080";
   }
-  return "https://game.teexit.maciek.codes";
+  return "https://teexit.maciek.codes:8080";
 };

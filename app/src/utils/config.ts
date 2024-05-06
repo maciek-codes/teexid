033b39bb85a602a2016bbc7dd09fd8d49a3fee6e
Maciek Kumorek <mkumorek@gmail.com>
2024-05-06 00:50:54 -0700

    remove 8080 in prod

--- app/src/utils/config.ts
+++ app/src/utils/config.ts
@@ -2,12 +2,12 @@ export const getWsHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "ws://localhost:4000/ws";
   }
-  return "wss://teexit.maciek.codes:8080/server/ws";
+  return "wss://teexit.maciek.codes/server/ws";
 };
 
 export const getHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "http://localhost:8080";
   }
-  return "https://teexit.maciek.codes:8080/server";
+  return "https://teexit.maciek.codes/server";
 };

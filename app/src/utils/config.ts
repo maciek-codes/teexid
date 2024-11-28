5eaf4bc769096d21f8fe192ab7389278565c82eb
Maciek Kumorek <mkumorek@gmail.com>
2024-11-28 10:15:14 -0800

    Fix the domain, set NODE_ENV

--- app/src/utils/config.ts
+++ app/src/utils/config.ts
@@ -2,12 +2,12 @@ export const getWsHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "ws://localhost:4000/ws";
   }
-  return "wss://teexit.maciek.codes/server/ws";
+  return "wss://dixit.maciek.codes/api/ws";
 };
 
 export const getHost = (): string => {
   if (process.env.NODE_ENV !== "production") {
     return "http://localhost:8080";
   }
-  return "https://teexit.maciek.codes/server";
+  return "https://dixit.maciek.codes/api";
 };
